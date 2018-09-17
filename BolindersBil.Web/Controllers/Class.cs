40541859634ffa72c56b4cc36da6d069a60db5f9
malloryfraiche <malloryfraiche@gmail.com>
2018-09-17 14:16:04 +0200

    added a class in the controller folder

---
+++ BolindersBil.Web/Controllers/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BolindersBil.Web.Controllers
+{
+    public class Class
+    {
+    }
+}
