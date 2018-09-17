371b9f5c502adfc427e8a05a3274242e10e56293
malloryfraiche <malloryfraiche@gmail.com>
2018-09-17 15:36:44 +0200

    Added references to class libraries. Added DB connections. Added Model classes. Added services in Startup.cs.

---
+++ BolindersBil.Models/Admin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BolindersBil.Models
+{
+    public class Admin
+    {
+    }
+}
--- BolindersBil.Web/Startup.cs
+++ BolindersBil.Web/Startup.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BolindersBil.Web.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BolindersBil.Web
 {
     public class Startup
     {
+        IConfiguration _configuration;
+        public Startup(IConfiguration conf)
+        {
+            _configuration = conf;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configuration for DB connection.
+            var conn = _configuration.GetConnectionString("BolindersBil");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(conn));
+
             services.AddMvc();
         }
 
@@ -24,6 +37,7 @@ namespace BolindersBil.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //app.UseStatusCodePages();
             }
 
             // To get access to the wwwroot files...
@@ -37,6 +51,8 @@ namespace BolindersBil.Web
             //        name: "default",
             //        template: "{controller=Home}/{action=Index}/{id?}");
             //});
+
+            //Seed.FillIfEmpty(ctx);
         }
     }
 }
