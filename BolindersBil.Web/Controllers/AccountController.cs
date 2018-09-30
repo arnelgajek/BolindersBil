418a96757087b3dd2015f5bc1ff8f60c7c1b5d1e
malloryfraiche <malloryfraiche@gmail.com>
2018-09-30 16:45:34 +0200

    Downloaded: NuGetPackage System.Drawing.Common. Added: Code to be able to take the uploaded image entered by the user and resize the image to one of size 80.  Saves the original image and the resized image in the Solution under the wwwroot folder images.

--- BolindersBil.Web/Controllers/AccountController.cs
+++ BolindersBil.Web/Controllers/AccountController.cs
@@ -1,14 +1,21 @@
 ﻿using BolindersBil.Models;
 using BolindersBil.Repositories;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+
 
 namespace BolindersBil.Web.Controllers
 {
@@ -16,15 +23,16 @@ namespace BolindersBil.Web.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IHostingEnvironment _hostingEnvironment;
         private IVehicleRepository vehicleRepo;
-        
 
-        public AccountController(IVehicleRepository vehicleRepository, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+
+        public AccountController(IVehicleRepository vehicleRepository, IHostingEnvironment hostingEnvironment, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             vehicleRepo = vehicleRepository;
+            _hostingEnvironment = hostingEnvironment;
             _userManager = userManager;
             _signInManager = signInManager;
-            vehicleRepo = vehicleRepository;
         }
 
         [AllowAnonymous]
@@ -149,39 +157,118 @@ namespace BolindersBil.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddNewVehicle(Vehicle addNewVehicle, List<IFormFile> Picture)
+        public async Task<IActionResult> AddNewVehicle(Vehicle addNewVehicle, IFormFile uploadedImage)
         {
             if (ModelState.IsValid && addNewVehicle != null)
             {
-                // To add the uploaded images into the Picture property of Vehicle.
-                foreach (var item in Picture)
+
+
+                // ****To take the uploaded photo and resize it to the same size (80)****
+                string imageFolder = "Vehicle_Images";
+                string targetFilename = uploadedImage.FileName;
+                // Get the path.
+                string webrootPath = _hostingEnvironment.WebRootPath;
+                string pathOfTargetFolder = webrootPath + "\\images\\" + imageFolder + "\\";
+                string fileTargetOriginal = pathOfTargetFolder + "\\Original\\" + targetFilename;
+                //string originalFilenameImage = pathOfTargetFolder + uploadedImage.FileName;
+
+                // Copy file to target.
+                using (var stream = new FileStream(fileTargetOriginal, FileMode.Create))
                 {
-                    if (item.Length > 0)
-                    {
-                        using (var stream = new MemoryStream())
-                        {
-                            await item.CopyToAsync(stream);
-                            addNewVehicle.Picture = stream.ToArray();
-                        }
-                    }
+                    await uploadedImage.CopyToAsync(stream);
                 }
-                addNewVehicle.AddedDate = DateTime.Now;
 
-               
+                // Resize and save the image under the folder named: 80. Calls on the ImageResize function.
+                ImageResize(fileTargetOriginal, pathOfTargetFolder + "\\80\\" + targetFilename, 80);
 
-                //var jkpg = addNewVehicle.OfficeId.OfficeCode;
-                //if (addNewVehicle.Office == "Jönköping")
+                // Output. 
+                ViewData["FileUploaded"] = "/images/Vehicle_Images/Original/" + targetFilename;
+                ViewData["FileResized_80"] = "/images/Vehicle_Images/80/" + targetFilename;
+
+
+
+
+                // addNewVehicle.Picture = ***The resized image that comes out of the ImageResize() function...
+
+
+
+                //// To add the uploaded images into the Picture property of Vehicle.
+                //foreach (var item in Picture)
                 //{
-                //    jkpg = "BB1";
-                //    addNewVehicle.OfficeId.OfficeCode= jkpg;
+                //    if (item.Length > 0)
+                //    {
+                //        using (var stream = new MemoryStream())
+                //        {
+                //            await item.CopyToAsync(stream);
+                //            addNewVehicle.Picture = stream.ToArray();
+                //        }
+                //    }
                 //}
 
+                addNewVehicle.AddedDate = DateTime.Now;
+
                 vehicleRepo.AddNewVehicle(addNewVehicle);
-                return View("TestVehicleAdded");
+
+                return View("TestVehicleAdded", ViewData);
             }
 
             return View();
         }
+
+        private void ImageResize(string inputImagePath, string outputImagePath, int newWidth)
+        {
+            const long quality = 50L;
+
+            Bitmap sourceBitmap = new Bitmap(inputImagePath);
+            double dblWidthOriginal = sourceBitmap.Width;
+            double dblHeigthOriginal = sourceBitmap.Height;
+            double heightWidthRelation = dblHeigthOriginal / dblWidthOriginal;
+            int newHeight = (int)(newWidth * heightWidthRelation);
+
+            // Create empty draw area.
+            var newDrawArea = new Bitmap(newWidth, newHeight);
+
+            using (var graphic_of_DrawArea = Graphics.FromImage(newDrawArea))
+            {
+                graphic_of_DrawArea.CompositingQuality = CompositingQuality.HighSpeed;
+                graphic_of_DrawArea.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphic_of_DrawArea.CompositingMode = CompositingMode.SourceCopy;
+
+                // Draw into placeholder.
+                // Imports the image into the drawarea.
+                graphic_of_DrawArea.DrawImage(sourceBitmap, 0, 0, newWidth, newHeight);
+                
+                // Output as .Jpg
+                using (var output = System.IO.File.Open(outputImagePath, FileMode.Create))
+                {
+                    // Setup jpg 
+                    var qualityParamId = Encoder.Quality;
+                    var encoderParameters = new EncoderParameters(1);
+                    encoderParameters.Param[0] = new EncoderParameter(qualityParamId, quality);
+                    
+                    // Save Bitmap as Jpg 
+                    var codec = ImageCodecInfo.GetImageDecoders().FirstOrDefault(c => c.FormatID == ImageFormat.Jpeg.Guid);
+                    newDrawArea.Save(output, codec, encoderParameters);
+                    output.Close();
+                }
+                
+                graphic_of_DrawArea.Dispose();
+            }
+            sourceBitmap.Dispose();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         [HttpGet]
         public IActionResult EditVehicle(int vehicleId)
@@ -249,7 +336,7 @@ namespace BolindersBil.Web.Controllers
                 FuelTypes = fuelType,
                 Offices = theOffices
             };
-            
+
             return View(vm);
         }
 
