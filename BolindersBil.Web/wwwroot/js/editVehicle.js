b680702de87ada10f36a33f57d269e109656241f
malloryfraiche <malloryfraiche@gmail.com>
2018-10-09 10:55:21 +0200

    Comments on the Edit button functionality.

--- BolindersBil.Web/wwwroot/js/editVehicle.js
+++ BolindersBil.Web/wwwroot/js/editVehicle.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
-    
+
+    // Sends the checked items value to the Vehicle controller EditVehicle action with the items value.
     $('#editButton').on('click', function () {
         var checkedValue = $('.chk:checked').val();
         var url = '/Vehicle/EditVehicle?vehicleId=' + checkedValue;
