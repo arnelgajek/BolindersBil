94711063dfcb4db5e6933211b3771d27c76d1f67
Arnel Gajek <gaar1719@student.ju.se>
2018-10-11 16:46:06 +0200

    The carousel works correctly.

--- BolindersBil.Web/wwwroot/js/vehicleForSale.js
+++ BolindersBil.Web/wwwroot/js/vehicleForSale.js
@@ -1,5 +1,22 @@
 ﻿$(function () {
 
+    // Sets the carousel-item to active:
     $('.carousel-item').first().addClass('active');
 
+    // Find the img in the carousel-item:
+    var carouselElem = $('.carousel-item').find("img");
+    console.log(carouselElem);
+
+    // Find the id of the img and pass it on to the modal:
+    $('#vehicleModal').on('show.bs.modal', function (e) {
+        var activeElement = $('div.carousel-inner').find('.active');
+        console.log(activeElement);
+
+        var index = activeElement.index();
+        console.log(index);
+
+        var theSrc = $('.thumbs img').eq(index).attr('src');
+
+         $("#modalCarousel").find("img").attr("src", theSrc);
+    });
 });
